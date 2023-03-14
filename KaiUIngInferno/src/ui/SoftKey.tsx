2b5a21b9ff7f1636532fd115ea40b9589fefe1e8
Farooq Karimi Zadeh <fkz@riseup.net>
2023-03-14 07:11:05 +0330

    convert softkey to tsx

--- KaiUIngInferno/src/ui/SoftKey.tsx
+++ KaiUIngInferno/src/ui/SoftKey.tsx
@@ -1,29 +1,48 @@
 import { Component, createTextVNode } from "inferno";
 import "KaiUI/src/components/SoftKey/SoftKey.scss";
 
+interface IButtonProps {
+  id: string;
+  handleClick: (evt: any) => void;
+  icon?: string;
+  text: string;
+}
+
 const prefixCls = "kai-softkey";
-function Button({ id, handleClick, icon, text }) {
-  let renderedIcon;
-  if (icon && icon.toString.indexOf("kai-") === -1) {
-    renderedIcon = <img src={icon} width={20} height={20} alt="" />;
+function Button(props: IButtonProps) {'
+  let renderedIcon: InfernoVNode;
+  if (props.icon && props.icon.toString().indexOf("kai-") === -1) {
+    renderedIcon = <img src={props.icon} width={20} height={20} alt="" />;
   } else {
-    renderedIcon = <span className={icon} />;
+    renderedIcon = <span className={props.icon} />;
   }
   return (
     <button
-      id={id}
+      id={props.id}
       className={`${prefixCls}-btn`}
-      onClick={handleClick}
+      onClick={props.handleClick}
       data-icon={renderedIcon ? "true" : undefined}
       $HasNonKeyedChildren
     >
-      {renderedIcon} {createTextVNode(text)}
+      {renderedIcon} {createTextVNode(props.text)}
     </button>
   );
 }
 
-class SoftKey extends Component {
-  handleKeyDown = (evt) => {
+interface ISoftKeyProps {
+  leftCb?: () => void,
+  rightCb?: () => void,
+  centerCb?: () => void,
+  centerText?: string,
+  leftText?: string,
+  rightText?: string,
+  rightIcon?: string,
+  centerIcon?: string,
+  leftIcon?: string
+}
+
+class SoftKey extends Component<ISoftKeyProps> {
+  handleKeyDown = (evt: KeyboardEvent | { key: string }) => {
     const { centerCb, rightCb, leftCb } = this.props;
     switch (evt.key) {
       case "Enter":
