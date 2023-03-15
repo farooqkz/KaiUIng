1007164cbc6cd2323758dd8769f9a50d298e026b
Farooq Karimi Zadeh <fkz@riseup.net>
2023-03-15 15:57:25 +0330

    trying to convert to tsx

---
+++ KaiUIngInferno/src/views/TabView.tsx
@@ -0,0 +1,78 @@
+import { Component } from "inferno";
+import Tabs from "../ui/Tabs";
+import Tab from "../ui/Tab";
+import "KaiUI/src/views/TabView/TabView.scss";
+import morecolors from "../morecolor";
+
+const prefixCls = "kai-tab-view";
+const tabViewTabs = `${prefixCls}-tabs`;
+const tabViewContent = `${prefixCls}-content`;
+
+type TabViewProps = {
+  onChangeIndex?: (number) => void;
+  focusColor?: string;
+  tabLabels: Array<string>;
+  defaultActiveTab?: number;
+};
+
+interface ITabViewState {
+  activeTab: number;
+};
+
+class TabView extends Component<TabViewProps> {
+  public focusColor: string;
+  public tabs: Array<JSX.Element>;
+  public state: ITabViewState;
+
+  handleChangeIndex = (tabIndex: number) => {
+    this.setState({ activeTab: tabIndex });
+    if (this.props.onChangeIndex) this.props.onChangeIndex(tabIndex);
+  };
+
+  constructor(props: TabViewProps) {
+    super(props);
+    const { defaultActiveTab, focusColor } = props;
+    this.focusColor = focusColor || morecolors.defaultFocusColor;
+    this.tabs = this.props.tabLabels.map((label) => (
+      <Tab label={label} focusColor={this.props.focusColor} key={label} />
+    ));
+    //this.state = window.stateStores.get("TabView") || { activeTab: 0 };
+    this.state = {
+      activeTab: defaultActiveTab || 0,
+    };
+    this.tabs[this.state.activeTab].props.isActive = true;
+  }
+
+  componentWillUnmount() {
+    //window.stateStores.set("TabView", this.state);
+  }
+
+  render() {
+    return (
+      <div className={prefixCls}>
+        <div className={tabViewTabs}>
+          <Tabs
+            defaultActiveChild={this.state.activeTab}
+            onChangeIndex={this.handleChangeIndex}
+            $HasKeyedChildren
+          >
+            {this.tabs}
+          </Tabs>
+        </div>
+        <div className={tabViewContent} $HasVNodeChildren>
+          {
+            this.props.children
+              .map(
+                (content, index) =>
+                  index === this.state.activeTab ? content : null,
+                this
+              )
+              .filter((content) => Boolean(content))[0]
+          }
+        </div>
+      </div>
+    );
+  }
+}
+
+export default TabView;
