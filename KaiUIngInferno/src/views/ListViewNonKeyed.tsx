3117fa9e0e1ee9faee657d900e790c5dbe981144
Farooq Karimi Zadeh <fkz@riseup.net>
2023-03-28 22:12:35 +0330

    add keyed and non keyed variants of LV

---
+++ KaiUIngInferno/src/views/ListViewNonKeyed.tsx
@@ -0,0 +1,106 @@
+import "KaiUI/src/views/ListView/ListView.scss";
+import { Component, VNode } from "inferno";
+import { findDOMNode } from "inferno-extras";
+import { asArray } from "../utils";
+
+interface IListViewState {
+  cursor: number;
+}
+
+interface IListViewProps {
+  children: any;
+  cursor: number;
+  cursorChangeCb?: (index: number) => void;
+  height?: number | string;
+  captureKeys?: Array<string>;
+}
+
+class ListViewNonKeyed extends Component<IListViewProps> {
+  public state: IListViewState;
+
+  handleKeyDown = (evt: KeyboardEvent) => {
+    let cursor = this.state.cursor;
+    const { cursorChangeCb, captureKeys } = this.props;
+    let children = asArray(this.props.children);
+
+    if (captureKeys instanceof Array && captureKeys.includes(evt.key)) {
+      evt.stopImmediatePropagation();
+    }
+    if (evt.key === "ArrowUp") {
+      cursor--;
+    } else if (evt.key === "ArrowDown") {
+      cursor++;
+    }
+    cursor += children.length;
+    cursor %= children.length;
+    this.setState({
+      cursor: cursor,
+    });
+    cursorChangeCb && cursorChangeCb(cursor);
+  };
+
+  constructor(props: IListViewProps) {
+    super(props);
+    const { cursor, cursorChangeCb } = props;
+    let children = asArray(props.children);
+    if (cursor - 1 > children.length || cursor < 0) {
+      console.error(
+        `[ListView] cursor should be from 0 to ${
+          children.length - 1
+        } but is ${cursor}`
+      );
+      throw new Error("cursor is negative or bigger than length of list");
+    }
+    if (cursorChangeCb) cursorChangeCb(cursor);
+    this.state = {
+      cursor: cursor,
+    };
+  }
+
+  componentDidUpdate() {
+    const { children } = this.props;
+    const { cursor } = this.state;
+    let node: HTMLElement | null = findDOMNode(asArray(children)[cursor]) as HTMLElement;
+    if (node) {
+      node.scrollIntoView({ block: "center" });
+    }
+  }
+
+  componentDidMount() {
+    document.addEventListener(
+      "keydown",
+      this.handleKeyDown,
+      Boolean(this.props.captureKeys)
+    );
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener(
+      "keydown",
+      this.handleKeyDown,
+      Boolean(this.props.captureKeys)
+    );
+  }
+
+  render() {
+    const { height } = this.props;
+    let children = asArray(this.props.children);
+    const { cursor } = this.state;
+    return (
+      <div
+        className={"kai-list-view"}
+        style={{
+          height: (height && height.toString()) || "calc(100vh - 60px)",
+        }}
+        $HasNonKeyedChildren
+      >
+        {children.map((child: VNode, index: number) => {
+          child.props.isFocused = index == cursor;
+          return child;
+        })}
+      </div>
+    );
+  }
+}
+
+export default ListViewNonKeyed;
